736a1a7a69806952a7b833accada3cdf85967d38
Natalia Silva <natalia.nsdeveloper@gmail.com>
2024-11-04 10:51:25 -0300

    clean project

--- projeto_final_ia/projeto_final_ia/urls.py
+++ projeto_final_ia/projeto_final_ia/urls.py
@@ -1,8 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from .views import color_prediction  # Importa a view de previsão de cor
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/predict-color/', color_prediction, name='color_prediction'),
 ]
